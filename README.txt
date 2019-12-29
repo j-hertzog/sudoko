84e017208b3b500baebae30fd9aaf9e3befc1792
Jeffrey Hertzog <jeffrey.g.hertzog@gmail.com>
2019-12-29 13:41:29 -0800

    Implemented the horizontal nonet sum functionality inside of the checkRC function. Changed the name of the checkRC method to checkRCN. Finished current to-do list and cleaned up code. Added example_5.txt to handle extraneous unique number input(such as 0 or negative #'s).

--- README.txt
+++ README.txt
@@ -2,3 +2,24 @@ Welcome to Sudoko, a lightweight Sudoku puzzle verifier.
   -parses and verifies a textfile with a "completed" sudoku table.
   -splits 9x9 table into 3, 3x3 tables to check if #'s are unique.
 
+  To run this program: 
+    -execute main with the text file of your choice
+
+    ./main example_1.txt
+
+  Example List:
+    example_1.txt
+      -complete and correct sudoku table 
+
+    example_2.txt
+      -incomplete sudoku table(empty spaces)
+
+    example_3.txt
+      -incorrect sudoku table(duplicate items)(rows)
+        
+    example_4.txt
+      -incorrect sudoku table(duplicate items)(columns)
+    
+    example_5.txt
+      -incorrect sudoku table(nonet sum is not 45) 
+      -0 still counts as a unique #, but makes the sum < 45
--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include <unordered_set>
 
 using namespace std;
 
 //prototypes
 bool checkTable(string n);
-bool checkRC(string n);
+bool checkRCN(string n);
 bool sumNonets(string n, int sum);
 
 int main(int argc, char* argv[])
 {
-  //verify correct input
+  //verify correct inputs
   if(argc != 2)
   {
     cout << "Error: Please enter the name of the text file you want to validate" << endl;
@@ -24,10 +23,11 @@ int main(int argc, char* argv[])
     cout << "Error: Incorrect grid format, please enter a 9x9 Sudoku grid" << endl;
     return 1;
   }
-  
-  //check for duplicates or spaces in rows and columns
-  checkRC(argv[1]);
-
+  //check if the #'s in row, columns, and nonets are correct
+  if(checkRCN(argv[1]))
+  {
+    cout << "Success, this sudoku table is correct!" << endl; 
+  }
   return 0;
 }
 
@@ -62,8 +62,8 @@ bool checkTable(string n)
     return true;
 }
 
-//verifies unique values in rows/columns
-bool checkRC(string n)
+//checks for unique items in each row/column and verifies that the sum of each nonet is 45
+bool checkRCN(string n)
 {
   ifstream mfile(n);
   if(mfile.fail())
@@ -71,19 +71,20 @@ bool checkRC(string n)
 
   int row = 0;
   int table[9][9];
-
+  
+  //pipes file input into 2d array and checks for unfilled spaces
   while(!mfile.fail())
   {
     string line;
     getline(mfile, line);
 
-    //prevent bad input
+    //prevents bad input
     if(mfile.fail())
       break;
 
     for(int i = 0; i < 9; i++)
     {
-      //checks for spaces while populating 2d array
+      //check for spaces 
       if(line[i] == ' ')
       {
         cout << "Error: Incomplete list, please fill in any empty spaces." << endl;
@@ -91,13 +92,15 @@ bool checkRC(string n)
       }
       else
       {
-          table[row][i] = line[i] - 48; // -48 for correct ascii to int conversion
-          cout << line[i] << " ";
+        table[row][i] = line[i] - 48; // -48 for correct ascii to int conversion
       }
     }
-    cout << endl;
     row++;
   }
+ 
+  //for checking the sum of each horizontal nonet(3x3 grid)
+  int sum1 = 0, sum2 = 0, sum3 = 0; 
+
   for(int i = 0; i < 9; i++)
   {
     unordered_set<int> r;
@@ -105,8 +108,7 @@ bool checkRC(string n)
       
     for(int j = 0; j < 9; j++)
     {
-      //checking for unique values using 2 unordered sets
-      //one for each orientation (row-wise) and (column-wise)
+      //checking for unique values using 2 unordered sets (row-wise) and (column-wise)
       if(r.find(table[i][j]) == r.end() && c.find(table[j][i]) == c.end())
       {
         r.insert(table[i][j]);
@@ -117,42 +119,33 @@ bool checkRC(string n)
         cout << "Sudoku table incorrect: duplicate items in the same row or column" << endl;
         return false;
       }
+
+      //finding the sum of each # in a nonet
+      if(j < 3)
+        sum1 += table[i][j];
+      else if(j < 6)
+        sum2 += table[i][j];
+      else
+        sum3 += table[i][j];
+    }
+    //after we reach rows 3, 6, and 9 check and clear the sums 
+    if((i+1)%3 == 0)
+    {
+      //check and clear sums
+      if(sum1 != 45 || sum2 != 45 || sum3 != 45)
+      {
+        cout << "Sudoku table incorrect: the sum a 3x3 grid is not 45" << endl;
+        return false;
+      }
+      else
+      {
+        //reset the sums for next three nonets
+        sum1 = 0;
+        sum2 =0;
+        sum3 = 0;
+      }
     }
   }
   return true;
 }
 
-bool sumNonets(string n, int sum)
-{
-  ifstream nfile(n);
-  
-  if(nfile.fail())
-    return false;
-
-  int count = 0;
-  int sum1 = 0;
-  int sum2 = 0;
-  int sum3 = 0;
-
-  while(!nfile.fail())
-  {
-    string line;
-    getline(nfile, line);
-
-    if(nfile.fail())
-      break;
-    
-   if(count != 0 && count%3 ==0)
-   {
-     sum1 = 0;
-     sum2 = 0;
-     sum3 = 0;
-   }
-  }
-
-
-
-
-
-
-}
